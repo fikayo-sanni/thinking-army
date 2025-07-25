9c60e7f8db8f851eb90311312a864b273c3b7b24
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2025-07-25 07:01:40 +0100

    feat: update logo

--- components/auth/auth-logo.tsx
+++ components/auth/auth-logo.tsx
@@ -4,7 +4,17 @@ export function AuthLogo() {
   return (
     <div className="flex flex-col items-center space-y-3">
       <div className="h-12 w-12 rounded-lg flex items-center justify-center">
-        <img src="/logo-dark-mode.svg" alt="Gamescoin Logo" className="h-20 w-20" />
+        <img
+          src="/logo-dark-mode.svg"
+          alt="GC Universe Logo"
+          className="h-20 w-20 hidden dark:block"
+        />
+        {/* Light Mode Logo */}
+        <img
+          src="/logo-light-mode.svg"
+          alt="GC Universe Logo"
+          className="h-20 w-20 dark:hidden"
+        />
       </div>
       <div>
         <h1 className="text-2xl font-bold text-white tracking-wide">GC UNIVERSE</h1>
